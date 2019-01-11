ae79b9180892dd161ff3adfb1a91b5de3e012fda
Anton Zdanov <azdanov@pm.me>
2019-01-11 20:52:42 +0200

    feat(app): add artisan auth scaffold

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+use function view;
+
+final class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(): View
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 declare(strict_types=1);
 
+Auth::routes();
+
 Route::get('/', 'WelcomeController@index');
 
 Route::get('events', 'EventsController@index')->name('events.index');
 Route::get('events/{id}', 'EventsController@show')->name('events.show');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
