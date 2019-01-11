f7802daba25d7776554b6efb2b68c9b8f16adb5a
Anton Zdanov <azdanov@pm.me>
2019-01-11 06:25:34 +0200

    feat(views): add css and js to index

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,10 +1,18 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Welcome to LaraMeet</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
+
 <h1>Welcome to LaraMeet</h1>
+
+<script src="{{ mix('js/app.js') }}"></script>
+
 </body>
+
 </html>
