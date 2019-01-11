625b4ba4d3bd806abe15a5b8e4b509f8a4b459ba
Anton Zdanov <azdanov@pm.me>
2019-01-11 04:57:04 +0200

    feat(database): add type hinting

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,19 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(App\User::class, function (Faker $faker) {
+/** @var Factory $factory */
+$factory->define(App\User::class, static function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
