3e2afe5d2faed0e59aed9754213722df8d27493a
Anton Zdanov <azdanov@pm.me>
2019-01-11 20:45:59 +0200

    feat(views): use Html Collective

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to LaraMeet</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    {!! Html::style('css/app.css') !!}
+    {!! Html::script('js/app.js', ['defer']) !!}
 </head>
 
 <body>
