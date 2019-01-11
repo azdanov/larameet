e9917e209785166091db0e2f23520c9aab294b59
Anton Zdanov <azdanov@pm.me>
2019-01-11 20:31:55 +0200

    chore(routes): cleanup

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,7 @@
 
 declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'WelcomeController@index');
+
+Route::get('events', 'EventsController@index')->name('events.index');
+Route::get('events/{id}', 'EventsController@show')->name('events.show');
