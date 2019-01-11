5d6f2b5db3d6c6a30c7f295d6ec788a35f8f09fc
Anton Zdanov <azdanov@pm.me>
2019-01-11 05:39:03 +0200

    chore(ide-helper): publish config, add script

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-01-11 02:26:25.
+    * Generated on 2019-01-11 03:38:12.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
