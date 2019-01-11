0a8cc7a3d361cf759493189b316ad8f0fa5067f7
Anton Zdanov <azdanov@pm.me>
2019-01-11 23:33:22 +0200

    feat(routes): add view routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 Auth::routes();
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'WelcomeController@index')->name('welcome');
+
+Route::view('about', 'about.index')->name('about.index');
+Route::view('about/faq', 'about.faq')->name('about.faq');
+Route::view('about/privacy', 'about.privacy')->name('about.privacy');
+Route::view('about/tos', 'about.tos')->name('about.tos');
+
+Route::view('contact', 'contact.index')->name('contact.index');
 
 Route::get('events', 'EventsController@index')->name('events.index');
 Route::get('events/{id}', 'EventsController@show')->name('events.show');
 
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('languages', 'LanguagesController@index')->name('languages.index');
+Route::get('locations', 'LocationsController@index')->name('locations.index');
+Route::get('map', 'MapsController@index')->name('maps.index');
