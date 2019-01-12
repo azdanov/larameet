6fc7eaf67cb980dcbc7abba923853e5fc61607f1
Anton Zdanov <azdanov@pm.me>
2019-01-12 23:50:58 +0200

    feat(views): add tailwind pagination

---
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -0,0 +1,31 @@
+@if ($paginator->hasPages())
+    <ul class="flex list-reset font-bold">
+
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="disabled">
+                <span
+                    class="border py-2 px-4 rounded-l text-indigo-light cursor-not-allowed shadow-inner">@lang('pagination.previous')</span>
+            </li>
+        @else
+            <li>
+                <a class="border py-2 px-4 rounded-l no-underline text-indigo-dark"
+                   href="{{ $paginator->previousPageUrl() }}"
+                   rel="prev">@lang('pagination.previous')</a>
+            </li>
+        @endif
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li>
+                <a class="border border-l-0 py-2 px-4 rounded-r no-underline text-indigo-dark"
+                   href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a>
+            </li>
+        @else
+            <li class="disabled ">
+                <span
+                    class="border border-l-0 py-2 px-4 rounded-r text-indigo-light cursor-not-allowed shadow-inner">@lang('pagination.next')</span>
+            </li>
+        @endif
+    </ul>
+@endif
---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,51 @@
+@php
+/** @var mixed[] $elements */
+/** @var mixed[]|string $element */
+@endphp
+
+@if ($paginator->hasPages())
+    <div class="flex items-center">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <span
+                class="rounded-l rounded-sm border border-r-0 px-3 py-1 cursor-not-allowed no-underline text-indigo-light">&laquo;</span>
+        @else
+            <a
+                class="rounded-l rounded-sm border border-r-0 px-3 py-1 text-brand-dark hover:bg-brand-light no-underline text-indigo-dark"
+                href="{{ $paginator->previousPageUrl() }}"
+                rel="prev"
+            >
+                &laquo;
+            </a>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <span
+                    class="border border-r-0 px-3 py-1 cursor-not-allowed no-underline text-indigo-dark">{{ $element }}</span>
+            {{-- Array Of Links --}}
+            @elseif (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page === $paginator->currentPage())
+                        <span
+                            class="border border-r-0 px-3 py-1 bg-brand-light no-underline text-indigo-darker shadow-inner">{{ $page }}</span>
+                    @else
+                        <a class="border border-r-0 px-3 py-1 hover:bg-brand-light text-brand-dark no-underline text-indigo-dark"
+                           href="{{ $url }}">{{ $page }}</a>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a class="rounded-r rounded-sm border px-3 py-1 hover:bg-brand-light text-brand-dark no-underline text-indigo-dark"
+               href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a>
+        @else
+            <span
+                class="rounded-r rounded-sm border px-3 py-1 hover:bg-brand-light text-brand-dark no-underline cursor-not-allowed text-indigo-light">&raquo;</span>
+        @endif
+    </div>
+@endif
