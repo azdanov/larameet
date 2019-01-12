f9aaa4e38125d189dbc030f76191b504651c7105
Anton Zdanov <azdanov@pm.me>
2019-01-12 22:09:05 +0200

    feat(migrations): add event migration

---
+++ database/migrations/2019_01_12_191530_create_events_table.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+final class CreateEventsTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('events', static function (Blueprint $table): void {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('venue');
+            $table->string('city');
+            $table->text('description')->nullable();
+            $table->boolean('enabled');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+}
