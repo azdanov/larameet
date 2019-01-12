c9c9b33b38837d4dc7459868a0c9ea71b72d962c
Anton Zdanov <azdanov@pm.me>
2019-01-12 23:51:35 +0200

    feat(Controllers): update EventsController

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -4,29 +4,68 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Event;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
-use function compact;
-use function date;
 use function view;
 
 final class EventsController extends Controller
 {
     public function index(): View
     {
-        $events = [
-            'Laravel Hacking and Coffee',
-            'IoT with Raspberry Pi',
-            'Free Vue.js Lessons',
-        ];
+        $events = Event::paginate(10);
 
         return view('events.index')->with('events', $events);
     }
 
-    public function show(string $id): View
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
     {
-        $name = 'Hilton Head Beach House';
-        $date = date('Y-m-d');
+    }
 
-        return view('events.show')->with(compact('id', 'name', 'date'));
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Event $event): View
+    {
+        return view('events.show')->with('event', $event);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @return Response
+     */
+    public function edit(Event $event)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @return Response
+     */
+    public function update(Request $request, Event $event)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return Response
+     */
+    public function destroy(Event $event)
+    {
     }
 }
