c62e4376b48909eebe2acc24001aaa4977965339
Anton Zdanov <azdanov@pm.me>
2019-01-12 19:52:44 +0200

    build(composer): update lock

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-01-11 21:57:36.
+    * Generated on 2019-01-12 17:52:08.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
