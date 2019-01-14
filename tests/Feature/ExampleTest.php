92adb44b04d0bc44d297df43c2f791301f3627b9
Anton Zdanov <azdanov@pm.me>
2019-01-14 17:25:35 +0200

    chore(tests): update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 final class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase;
+
     public function testBasicTest(): void
     {
         $response = $this->get('/');
