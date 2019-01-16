4d029d3c106ba0fc639b283a8e0f42f46cd06764
Anton Zdanov <azdanov@pm.me>
2019-01-16 21:39:18 +0200

    feat(views): style events.index

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -10,20 +10,30 @@
 @endsection
 
 @section('content')
-    <h2>Currently Open:</h2>
-    <ul class="list-reset">
-        @forelse ($events as $event)
-            <li>
-                <a
-                    href="{{ route('events.show', $event->slug) }}"
-                    class="no-underline text-indigo-dark hover:underline"
-                >{{ $event->name }}</a>
-            </li>
-        @empty
-            <li>No events found!</li>
-        @endforelse
-    </ul>
-    <div class="mt-3 mb-1">
-        {!! $events->links('vendor.pagination.tailwind') !!}
-    </div>
+    <article class="lg:w-5/6 mx-auto">
+        <h2 class="mb-2">Currently Open</h2>
+        <ul class="list-reset flex flex-wrap">
+            @forelse ($events as $event)
+                <li class="mb-4 md:w-1/2">
+                    <a
+                        href="{{ route('events.show', $event->slug) }}"
+                        class="no-underline text-indigo-dark hover:underline"
+                    >{{ $event->name }}</a>
+                    <div class="flex items-center text-grey-darker">
+                        <svg class="w-5 h-6 mr-1" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                  d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z"></path>
+                        </svg>
+                        <span>{{ $event->venue }} ({{ $event->city }})</span>
+                    </div>
+                </li>
+            @empty
+                <li>No events found!</li>
+            @endforelse
+        </ul>
+        <div class="mt-1 mb-1">
+            {!! $events->links('vendor.pagination.tailwind') !!}
+        </div>
+    </article>
+
 @endsection
