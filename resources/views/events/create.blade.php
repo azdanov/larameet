8e435b751d6541f6bafeaed7d534e7b2c77dab44
Anton Zdanov <azdanov@pm.me>
2019-01-16 20:27:11 +0200

    feat(views): add create events view

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.main')
+
+@php
+    /* @var App\Event[] $events */
+@endphp
+
+@section('heading')
+    <h1>Events</h1>
+@endsection
+
+@section('content')
+    <div class="px-4 md:px-0 md:w-3/4 mx-auto my-3">
+        <h2 class="text-grey-darkest text-center mb-4">Create an event</h2>
+        {!! Form::open(['route' => 'events.store']) !!}
+        <div class="md:flex md:items-center mb-4">
+            <div class="md:w-1/3">
+                {!!Form::label('name', 'Event Name', [
+                    'class' => 'block text-grey-dark font-bold md:text-right mb-1 md:mb-0 pr-4' ])
+                !!}
+            </div>
+            <div class="md:w-2/3">
+                {!! Form::text('name', null, [
+                    'class' => 'bg-grey-lightest appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-indigo',
+                    'placeholder' => 'Laravel and Pancakes' ])
+                !!}
+            </div>
+        </div>
+        <div class="md:flex md:items-center mb-4">
+            <div class="md:w-1/3">
+                {!! Form::label('max_attendees', 'Maximum Number of Attendees (including organizers)', [
+                    'class' => 'block text-grey-dark font-bold md:text-right mb-1 md:mb-0 pr-4'])
+                !!}
+            </div>
+            <div class="md:w-2/3">
+                {!! Form::select('max_attendees', [2,3,4,5,6,7,8,9,10], null, [
+                    'placeholder' => 'Maximum Number of Attendees',
+                    'class' => 'bg-grey-lightest appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-indigo' ])
+                !!}
+            </div>
+        </div>
+        <div class="md:flex md:items-center mb-4">
+            <div class="md:w-1/3">
+                {!!Form::label('venue', 'Venue', [
+                    'class' => 'block text-grey-dark font-bold md:text-right mb-1 md:mb-0 pr-4' ])
+                !!}
+            </div>
+            <div class="md:w-2/3">
+                {!! Form::text('venue', null, [
+                    'class' => 'bg-grey-lightest appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-indigo',
+                    'placeholder' => 'Pancakes and Stars Cafe' ])
+                !!}
+            </div>
+        </div>
+        <div class="md:flex md:items-center mb-5">
+            <div class="md:w-1/3">
+                {!! Form::label('description', 'Description', [
+                    'class' => 'block text-grey-dark font-bold md:text-right mb-1 md:mb-0 pr-4'])
+                !!}
+            </div>
+            <div class="md:w-2/3">
+                {!! Form::textarea('description', null, [
+                    'placeholder' => 'Describe the event',
+                    'class' => 'bg-grey-lightest appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-indigo',
+                    'rows' => '4' ])
+                !!}
+            </div>
+        </div>
+        <div class="md:flex md:items-center">
+            <div class="md:w-1/3"></div>
+            <div class="md:w-2/3">
+                {!! Form::button('Submit', [
+                    'class' => 'shadow bg-indigo hover:bg-indigo-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded',
+                    'type' => 'submit' ])
+                !!}
+            </div>
+        </div>
+        {!! Form::close() !!}
+    </div>
+@endsection
