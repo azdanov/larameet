d9a0d4aaa722da2b2b94aae946555c9fbb40d42c
Anton Zdanov <azdanov@pm.me>
2019-01-16 20:24:44 +0200

    fix(views): properly add versioned files for dependencies

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,21 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to LaraMeet</title>
-    {!! Html::style('css/app.css') !!}
-    {!! Html::script('js/app.js', ['defer']) !!}
+    {!! Html::style(mix('css/app.css')) !!}
+    {!! Html::script(mix('js/app.js'), ['defer']) !!}
 </head>
 
 <body class="min-h-screen flex flex-col font-sans">
 @include('partials._menu')
 
 <main class="flex-grow">
-    <div class="pt-1 pb-3 bg-indigo-light text-center shadow">
+    <div class="pt-2 pb-3 bg-indigo-light text-center shadow">
         <div class="container mx-auto text-white leading-normal">
             @section('heading')
                 <h1>LaraMeet</h1>
             @show
         </div>
     </div>
+
+    @include('flash::message')
 
     <div class="container mx-auto py-3 leading-normal">
         @yield('content')
