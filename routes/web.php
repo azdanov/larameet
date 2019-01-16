4d0252e69b38d6ae397eb0c669877617faf88d45
Anton Zdanov <azdanov@pm.me>
2019-01-16 20:45:37 +0200

    feat(routes): add edit and update to events

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@ Route::view('about/tos', 'about.tos')->name('about.tos');
 Route::view('contact', 'contact.index')->name('contact.index');
 
 Route::resource('events', 'EventsController')
-    ->only(['index', 'show', 'create', 'store'])
+    ->only(['index', 'show', 'create', 'store', 'edit', 'update'])
     ->names([
         'index' => 'events.index',
         'show' => 'events.show',
         'create' => 'events.create',
         'store' => 'events.store',
+        'edit' => 'events.edit',
+        'update' => 'events.update',
     ]);
 
 Route::get('languages', 'LanguagesController@index')->name('languages.index');
