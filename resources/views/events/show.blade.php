38947740c206ac315b6b0b33bf668851a5a8599c
Anton Zdanov <azdanov@pm.me>
2019-01-16 21:06:49 +0200

    feat(views): add edit and delete links to events.show

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -9,12 +9,22 @@
 @endsection
 
 @section('content')
-    <article class="leading-loose">
+    <article class="leading-loose w-3/4 mx-auto">
         <section class="mb-1">
-            <h2>{{ $event->name }}</h2>
+            <div class="flex items-baseline text-indigo-dark">
+                <h2 class="text-indigo-darkest">{{ $event->name }}</h2>
+                <span class="mx-2">&dash;</span>
+                {{ link_to_route('events.edit', 'Edit Event', ['event' => $event],
+                    ['class' => 'no-underline text-indigo-dark hover:underline']) }}
+                <span class="mx-2">&dash;</span>
+                {!! Form::open(['route' => ['events.destroy', $event], 'method' => 'delete']) !!}
+                    {!! Form::button('Delete Event', ['class' => 'no-underline text-red-dark hover:underline', 'type' => 'submit']) !!}
+                {!! Form::close() !!}
+            </div>
             <p>City: {{ $event->city }}</p>
             <p>Venue: {{ $event->venue }}</p>
             <p>Status: {{ $event->published ? 'Open' : 'Closed' }}</p>
+            <p>Max Attendees: {{ $event->max_attendees }}</p>
         </section>
         <section>
             <h3>Description</h3>
