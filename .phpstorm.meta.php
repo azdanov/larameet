1e6e05ccfe1aa735d715b10f3bd7a6b227179858
Anton Zdanov <azdanov@pm.me>
2019-01-16 08:09:20 +0200

    chore(deps): update composer

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-01-14 08:53:27.
+    * Generated on 2019-01-16 06:09:10.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
