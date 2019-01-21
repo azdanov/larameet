9a8b262c34f718d2e910dc5fbc09b866f92904d0
Anton Zdanov <azdanov@pm.me>
2019-01-21 11:41:39 +0200

    feat(Requests): add auth check to EventFormRequest

--- app/Http/Requests/EventFormRequest.php
+++ app/Http/Requests/EventFormRequest.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 final class EventFormRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /** @return mixed[] */
